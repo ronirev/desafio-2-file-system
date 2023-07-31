c3719f1fbfc35e8a47352bcf29029c53c7a2ed2d
ronirev <ronihdev@gmail.com>
2023-07-31 08:26:21 -0300

    remove console logs

--- helpers/setPathData.js
+++ helpers/setPathData.js
@@ -6,16 +6,12 @@ import { promises as fs } from 'fs';
     const { ruta } = splitPathAndFileName(path)
     try {
         await fs.access(path);
-        console.log('Directorio y archivo ya existentes. No se reescribe el archivo.');
         return path;
     } catch (err) {
-        // El archivo no existe, lo creamos más adelante
     }
 
     await fs.mkdir(ruta, { recursive: true });
     await fs.writeFile(path, JSON.stringify(content));
-    console.log('Directorio y archivo creados.');
-
     return path;
 } catch (error) {
     console.error('Error al crear el directorio o archivo:', error);
@@ -29,7 +25,10 @@ const splitPathAndFileName = (filePath) =>{
   const ruta = filePath.slice(0, lastSlashIndex + 1);
   const fileName = filePath.slice(lastSlashIndex + 1);
 
-  return { ruta, fileName };
+  return { 
+    ruta, 
+    fileName 
+};
 }
 
 
--- main.js
+++ main.js
@@ -8,29 +8,29 @@ const product = new Product('Producto1','Test',10,'Test','CODPROD01',12);
 
 
 //Test addProduct:
- // manager.addProduct(product);
+  //console.log(await manager.addProduct(product));
 
 //Test getProducts:
  // console.log(await manager.getProducts());
 
 //Test getProductById:
- // console.log(await manager.getProductByid(2))
+  //console.log(await manager.getProductByid(2))
 
 //Test deleteById:
    // console.log(await manager.deleteById(1))
+   // console.log(await manager.getProducts())
 
 //Test deleteAll:     
-    //manager.deleteAll()
+ //   manager.deleteAll()
+ //  console.log(await manager.getProducts())
 
     
 // Test Update : 
-const productToUpdated = await manager.getProductByid(1)
-console.log(productToUpdated) 
+//const productToUpdated = await manager.getProductByid(1)
 //productToUpdated.description = 'Description updated'
-//console.log(productToUpdated) 
-//manager.updateProduct(productToUpdated)
+//await manager.updateProduct(productToUpdated)
 //console.log(await manager.getProductByid(1))
 
 
-//manager.testEscritura('data')
+
 
